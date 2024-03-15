ddfa2a97bda2acb1da6ca2aeac6b1a2a2d7dc4d1
TylorDev <jhontmg26@gmail.com>
2024-03-15 15:46:41 -0300

    refactor end

--- Original/app.js
+++ Original/app.js
@@ -1,26 +1,21 @@
-"use strict";
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLink = document.querySelectorAll(".nav-links li");
 
-const navSlide = ()=>{
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".nav-links");
-    const navLink = document.querySelectorAll(".nav-links li")
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
 
-    burger.addEventListener("click",()=>{
-        nav.classList.toggle("nav-active");
-
-    
-        navLink.forEach((link, index)=> {
-            if(link.style.animation){
-            link.style.animation = "";
-            }
-            else {
-            link.style.animation= `navLinkFade 0.5s ease forwards ${index/7 + 0.5}s`;
-         }
-        });
-        burger.classList.toggle("toggle");
+    navLink.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
     });
-
-   
-
-}
+    burger.classList.toggle("toggle");
+  });
+};
 navSlide();
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.scss";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Nav></Nav>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+const Nav = () => {
+  const [navActive, setNavActive] = useState(false);
+  const [navLink, setNavlink] = useState(null);
+
+  useEffect(() => {
+    const navLinks = document.querySelectorAll(".nav-links > li");
+    setNavlink(navLinks);
+    console.log(navLink);
+  }, []);
+
+  const toggleNav = () => {
+    setNavActive(!navActive);
+    navLink.forEach((link, index) => {
+      link.style.animation = "";
+      link.style.animation = `navLinkFade 0.3s ease forwards ${
+        index / 7 + 0.5
+      }s`;
+    });
+  };
+
+  return (
+    <nav>
+      <div className="Logo">
+        <h4>The Nav</h4>
+      </div>
+      <ul className={navActive ? "nav-links nav-active" : "nav-links"}>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Work</a>
+        </li>
+        <li>
+          <a href="#">Projects</a>
+        </li>
+      </ul>
+
+      <div
+        className={navActive ? "burger toggle" : "burger"}
+        onClick={toggleNav}
+      >
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
+    </nav>
+  );
+};
